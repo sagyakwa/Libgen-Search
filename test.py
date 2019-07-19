3d6bc3cbecb0033fa2418a4286972fb7b39e2a3e
asagyakwa <samuelagyen@gmail.com>
2019-07-19 15:00:52 -0400

    added more test cases

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import lxml
 
-home_page = urllib.request.urlopen('http://gen.lib.rus.ec/search.php?req=978-1285741550&open=0&res=25&view=simple&phrase=1&column=def')
+home_page = urllib.request.urlopen('http://93.174.95.29/_ads/7D475D8B3A0A1A8F1002A68EF2E8FAD7')
 soup = BeautifulSoup(home_page)
-ip_format = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
-for links in soup.find_all('a', attrs={'href': [re.compile("(^http://93.174.95.29)"), re.compile("^http://libgen.io/get"), re.compile("^http://libgen.me")]}):
-    print(links.get('href'))
+# for dl_links in soup.find_all('a', attrs={
+#                 'href': re.compile("^http://")}):
+#     print(dl_links.get('href'))
+
+for dl_links in soup.find_all('a', attrs={
+    'href': re.compile("^/main")}):
+    print(dl_links.get('href'))
 
 # for links in soup.find_all('a', attrs={'href': re.compile("^http://")}):
 #     print(links.get('href'))
